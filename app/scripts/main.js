07dbfcf8f201adf9cbf2094c5e6456427181a60d
Alberto Congiu <4lbertoC@gmail.com>
2015-03-07 16:00:42 +0100

    added click handlers to change pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,6 +13,9 @@ function init() {
 		var chapter = chapters[i];
 		processChapter(chapter, i);
 	}
+
+	$('.prevPage').on('click', prevPage);
+	$('.nextPage').on('click', nextPage);
 }
 
 function processChapter(chapter, chapterId) {
@@ -24,7 +27,7 @@ function processChapter(chapter, chapterId) {
 
 	var paragraphId = 0;
 
-
+	text = '<p>' + text;
 	text = text.replace(/\n\n[\n]*/g, '</p><p class="paragraph">');
 	text += '</p></div>'
 
@@ -41,8 +44,57 @@ function processChapter(chapter, chapterId) {
 	$chapter.unwrap();
 }
 
-function getNextPageScrollPosition() {
-	
+function nextPage() {
+	var topPos = $(window).scrollTop();
+	var screenHeight = $(window).height();
+
+	var nextParagraphTop = topPos + screenHeight;
+
+	var nextParagraph;
+	Array.prototype.some.call($('.paragraph'),function(p) {
+	  if($(p).offset().top > nextParagraphTop) {
+	    nextParagraph = p;
+	    return true;
+	   }
+	   return false;
+	});
+
+	var prevParagraph = getPrevParagraph(nextParagraph);
+
+	if(prevParagraph.size() > 0) {
+		$(window).scrollTop(prevParagraph.offset().top);
+	}
+}
+
+function prevPage() {
+	var topPos = $(window).scrollTop();
+	var screenHeight = $(window).height();
+
+	var prevParagraphTop = topPos - screenHeight;
+
+	var prevParagraph;
+	var reverseParagraphs = Array.prototype.reverse.call($('.paragraph'));
+	Array.prototype.some.call(reverseParagraphs,function(p) {
+	  if($(p).offset().top < prevParagraphTop) {
+	    prevParagraph = p;
+	    return true;
+	   }
+	   return false;
+	});
+
+	var prevParagraph2 = getPrevParagraph(prevParagraph);
+	if(prevParagraph2.size() > 0) {
+		$(window).scrollTop(prevParagraph2.offset().top);
+	}
+}
+
+function getPrevParagraph(paragraph) {
+	var $paragraph = $(paragraph);
+	var prevP = $paragraph.prev();
+	if(typeof prevP === 'undefined') {
+		prevP = prevP.parent().prev().prev().children().last();
+	}
+	return prevP || paragraph;
 }
 
 init();
